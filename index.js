0567f5f9d0756eb8cea4b6e519557bbac1ee895c
Geetam <geetambinani6@gmail.com>
2024-02-25 20:05:35 +0530

    remove console statement

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-import dotenv from "dotenv";
-dotenv.config();
 export const getWeatherDetails = async (location) => {
-	const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${process.env.WEATHER_API_KEY}`;
+	const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=79487e10b9a362eec78787a28e30f6e5`;
 	const data = await fetch(URL);
 	const details = await data.json();
 
